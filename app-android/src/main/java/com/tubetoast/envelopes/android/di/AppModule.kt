f705f6c18a831a8d4578f190e6233ecc056b25dd
Khmelev Artem <artem.khmelev@myoffice.team>
2023-12-11 09:20:51 +0300

    delete AddSpendingViewModel.kt

--- app-android/src/main/java/com/tubetoast/envelopes/android/di/AppModule.kt
+++ app-android/src/main/java/com/tubetoast/envelopes/android/di/AppModule.kt
@@ -1,15 +1,12 @@
 package com.tubetoast.envelopes.android.di
 
-import com.tubetoast.envelopes.android.presentation.ui.screens.AddSpendingViewModel
 import com.tubetoast.envelopes.android.presentation.ui.screens.EditCategoryViewModel
 import com.tubetoast.envelopes.android.presentation.ui.screens.EditEnvelopeViewModel
 import com.tubetoast.envelopes.android.presentation.ui.screens.EnvelopesListViewModel
-import com.tubetoast.envelopes.android.util.JavaDateGenerator
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { AddSpendingViewModel(get(), JavaDateGenerator()) }
     viewModel { EnvelopesListViewModel(get(), get()) }
     viewModel { EditEnvelopeViewModel(get()) }
     viewModel { EditCategoryViewModel(get(), get()) }
--- app-common/src/main/java/com/tubetoast/envelopes/common/util/DateGenerator.kt
+++
@@ -1,7 +0,0 @@
-package com.tubetoast.envelopes.common.util
-
-import com.tubetoast.envelopes.common.domain.models.Date
-
-interface DateGenerator {
-    fun now(): Date
-}
