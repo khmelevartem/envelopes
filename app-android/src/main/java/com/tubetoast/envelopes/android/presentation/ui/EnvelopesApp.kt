4a2ef8b2bada14019596994fff738ad014f07aca
Artem <khmelyovartyom@gmail.com>
2023-12-27 00:17:36 +0500

    fix reapplying nav back stack args

--- app-android/src/main/java/com/tubetoast/envelopes/android/presentation/ui/EnvelopesApp.kt
+++ app-android/src/main/java/com/tubetoast/envelopes/android/presentation/ui/EnvelopesApp.kt
@@ -2,19 +2,19 @@ package com.tubetoast.envelopes.android.presentation.ui
 
 import android.os.Bundle
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.tubetoast.envelopes.android.presentation.ui.screens.*
-import com.tubetoast.envelopes.common.data.CategoriesRepositoryImpl
-import com.tubetoast.envelopes.common.data.EnvelopesRepositoryImpl
-import com.tubetoast.envelopes.common.data.SpendingRepositoryImpl
-import com.tubetoast.envelopes.common.domain.CategoryInteractorImpl
-import com.tubetoast.envelopes.common.domain.EnvelopeInteractorImpl
-import com.tubetoast.envelopes.common.domain.SnapshotsInteractorImpl
+import com.tubetoast.envelopes.android.presentation.ui.screens.EditCategoryScreen
+import com.tubetoast.envelopes.android.presentation.ui.screens.EditCategoryViewModel
+import com.tubetoast.envelopes.android.presentation.ui.screens.EditEnvelopeScreen
+import com.tubetoast.envelopes.android.presentation.ui.screens.EditEnvelopeViewModel
+import com.tubetoast.envelopes.android.presentation.ui.screens.EnvelopesListScreen
+import com.tubetoast.envelopes.android.presentation.ui.screens.EnvelopesListViewModel
 import com.tubetoast.envelopes.common.domain.models.Envelope
 
 private const val NO_VALUE = -1
@@ -26,71 +26,52 @@ fun EnvelopesApp(
     editCategoryViewModel: EditCategoryViewModel,
 ) {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
     NavHost(navController = navController, startDestination = AppNavigation.start) {
         composable(route = AppNavigation.envelopesList) {
             EnvelopesListScreen(navController, envelopesListViewModel)
         }
         composable(route = AppNavigation.addEnvelope) {
-            EditEnvelopeScreen(
-                navController,
-                editEnvelopeViewModel,
-            )
+            EditEnvelopeScreen(navController, editEnvelopeViewModel)
         }
         composable(
             route = AppNavigation.editEnvelope,
             arguments = listOf(
                 navArgument(AppNavigation.argEnvelopeHash) { type = NavType.IntType },
             ),
-        ) { navBackStackEntry ->
-            EditEnvelopeScreen(
-                navController,
-                editEnvelopeViewModel.also { vm ->
-                    navBackStackEntry.arguments?.takeIntAndRemove(AppNavigation.argEnvelopeHash) {
-                        vm.setEditedEnvelopeHash(it)
-                    }
-                },
-            )
+        ) {
+            navBackStackEntry?.arguments?.takeInt(AppNavigation.argEnvelopeHash)?.also {
+                editEnvelopeViewModel.setEditedEnvelopeHash(it)
+            }
+            EditEnvelopeScreen(navController, editEnvelopeViewModel)
         }
         composable(
             route = AppNavigation.addCategory,
             arguments = listOf(
                 navArgument(AppNavigation.argEnvelopeHash) { type = NavType.IntType },
             ),
-        ) { navBackStackEntry ->
-            EditCategoryScreen(
-                navController,
-                editCategoryViewModel.also { vm ->
-                    navBackStackEntry.arguments?.takeIntAndRemove(AppNavigation.argEnvelopeHash) {
-                        vm.setEnvelopeHash(it)
-                    }
-                },
-            )
+        ) {
+            navBackStackEntry?.arguments?.takeInt(AppNavigation.argEnvelopeHash)?.also {
+                editCategoryViewModel.setEnvelopeHash(it)
+            }
+            EditCategoryScreen(navController, editCategoryViewModel)
         }
         composable(
             route = AppNavigation.editCategory,
             arguments = listOf(
                 navArgument(AppNavigation.argCategoryHash) { type = NavType.IntType },
             ),
-        ) { navBackStackEntry ->
-            EditCategoryScreen(
-                navController,
-                editCategoryViewModel.also { vm ->
-                    navBackStackEntry.arguments?.takeIntAndRemove(AppNavigation.argCategoryHash) {
-                        vm.setEditedCategoryHash(it)
-                    }
-                },
-            )
+        ) {
+            navBackStackEntry?.arguments?.takeInt(AppNavigation.argCategoryHash)?.also {
+                editCategoryViewModel.setEditedCategoryHash(it)
+            }
+            EditCategoryScreen(navController, editCategoryViewModel)
         }
     }
 }
 
-private fun Bundle.takeIntAndRemove(key: String, action: (Int) -> Unit) {
-    getInt(key, NO_VALUE)
-        .takeUnless { it == NO_VALUE }
-        ?.also {
-            action(it)
-            remove(key)
-        }
+private fun Bundle.takeInt(key: String): Int? {
+    return getInt(key, NO_VALUE).takeUnless { it == NO_VALUE }
 }
 
 object AppNavigation {
