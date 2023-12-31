de7df0dcbcbab193b6b3db84ec99354e97a5face
Artem <khmelyovartyom@gmail.com>
2023-12-31 14:06:50 +0500

    make envelope id rely only on its name

--- app-common/src/main/java/com/tubetoast/envelopes/common/domain/models/Envelope.kt
+++ app-common/src/main/java/com/tubetoast/envelopes/common/domain/models/Envelope.kt
@@ -4,6 +4,9 @@ data class Envelope(
     val name: String,
     val limit: Amount,
 ) : ImmutableModel<Envelope>() {
+
+    override val id: Id<Envelope> = name.id()
+
     companion object {
         val EMPTY = Envelope(name = "", limit = Amount.ZERO)
     }
