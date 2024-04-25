2f95a2e425cbd5ca4390c202b6beeec1e5759de5
Artem <khmelyovartyom@gmail.com>
2024-04-25 23:07:12 +0300

    Date improvements

--- app-common/src/main/java/com/tubetoast/envelopes/common/domain/models/Date.kt
+++ app-common/src/main/java/com/tubetoast/envelopes/common/domain/models/Date.kt
@@ -1,12 +1,48 @@
 package com.tubetoast.envelopes.common.domain.models
 
+import java.util.Calendar
 import kotlin.random.Random
 
 data class Date(
     val day: Int,
     val month: Int,
     val year: Int
-) : ImmutableModel<Date>()
+) : ImmutableModel<Date>(), Comparable<Date> {
+    init {
+        if (month !in 1..12) throw IllegalArgumentException("month $month")
+        when (month) {
+            1, 3, 5, 7, 8, 10, 12 -> if (day !in 1..31) throw IllegalArgumentException("month $month day $day")
+            4, 6, 9, 11 -> if (day > 30) throw IllegalArgumentException("month $month day $day")
+            2 -> if (year.mod(4) == 0 && day > 29 || year.mod(4) != 0 && day > 28) throw IllegalArgumentException(
+                "month $month day $day"
+            )
+        }
+    }
+
+    override fun compareTo(other: Date): Int =
+        when {
+            this.year == other.year && this.month == other.month && this.day == other.day -> 0
+            this.year >= other.year && this.month >= other.month && this.day > other.day -> 1
+            else -> -1
+        }
+
+    companion object {
+        fun today(): Date =
+            date { get(Calendar.DAY_OF_MONTH) }
+
+        fun currentMonth(day: Int = 1) =
+            date { day }
+
+        private fun date(day: Calendar.() -> Int) =
+            Calendar.getInstance().run {
+                Date(
+                    day = day(),
+                    month = get(Calendar.MONTH) + 1,
+                    year = get(Calendar.YEAR),
+                )
+            }
+    }
+}
 
 fun randomDate() = Random(System.currentTimeMillis()).run {
     Date(day = nextInt(), month = nextInt(), year = nextInt())
