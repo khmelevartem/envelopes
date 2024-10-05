84746b92b167f807adcf9a7a2c95c484db23c3b6
Khmelev Artem <artem.khmelev@myoffice.team>
2024-10-06 01:42:50 +0300

    navigate to edit category from edit envelope screen

--- app-android/src/main/java/com/tubetoast/envelopes/android/presentation/ui/screens/EditEnvelopeScreen.kt
+++ app-android/src/main/java/com/tubetoast/envelopes/android/presentation/ui/screens/EditEnvelopeScreen.kt
@@ -3,6 +3,7 @@ package com.tubetoast.envelopes.android.presentation.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,7 +16,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.tubetoast.envelopes.android.presentation.ui.AppNavigation
 import com.tubetoast.envelopes.android.presentation.ui.views.CategoryWithSumView
 import com.tubetoast.envelopes.android.presentation.ui.views.EnvelopesTopAppBar
 import com.tubetoast.envelopes.android.presentation.ui.views.TopAppBarViewModel
@@ -64,8 +67,15 @@ fun EditEnvelopeScreen(
             }
         }
         LazyColumn {
-            items(categories) {
-                CategoryWithSumView(snapshot = it, onClick = {})
+            items(categories) { categorySnapshot ->
+                CategoryWithSumView(
+                    snapshot = categorySnapshot,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                ) {
+                    navController.navigate(
+                        AppNavigation.editCategory(categorySnapshot.category, draftEnvelope)
+                    )
+                }
             }
         }
     }
