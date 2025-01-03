43350f6869be4b2b22d090f01f940328572e6050
Khmelev Artem <artem.khmelev@myoffice.team>
2025-01-03 19:37:55 +0300

    compare DateRange

--- app-common/src/main/java/com/tubetoast/envelopes/common/domain/models/DateRange.kt
+++ app-common/src/main/java/com/tubetoast/envelopes/common/domain/models/DateRange.kt
@@ -3,4 +3,12 @@ package com.tubetoast.envelopes.common.domain.models
 data class DateRange(
     override val endInclusive: Date,
     override val start: Date
-) : ClosedRange<Date>
+) : ClosedRange<Date>, Comparable<DateRange> {
+    override fun compareTo(other: DateRange) = when { // less means earlier
+        other.start == start && other.endInclusive == endInclusive -> 0
+        other.start < start && other.endInclusive < endInclusive -> 1
+        other.start > start && other.endInclusive > endInclusive -> -1
+        else -> throw IllegalStateException("Cannot compare $this to $other")
+    }
+
+}
--- app-common/src/test/kotlin/com/tubetoast/envelopes/common/domain/models/DateRangeTest.kt
+++ app-common/src/test/kotlin/com/tubetoast/envelopes/common/domain/models/DateRangeTest.kt
@@ -2,6 +2,9 @@ package com.tubetoast.envelopes.common.domain.models
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class DateRangeTest {
     @Test
@@ -17,4 +20,44 @@ class DateRangeTest {
         val dateNotInRange = Date(1, 2, 2)
         Assertions.assertFalse(dateNotInRange !in dateRange)
     }
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    fun testCompare(range1: DateRange, range2: DateRange) {
+        Assertions.assertTrue(range1 < range2)
+        Assertions.assertTrue(range2 > range1)
+    }
+
+    @Test
+    fun testCompareEquals() {
+        val range1 = Date(1, 1, 1)..Date(3, 3, 3)
+        val range2 = Date(1, 1, 1)..Date(3, 3, 3)
+        Assertions.assertTrue(range1 == range2)
+    }
+
+    @Test
+    fun testCannotCompare() {
+        val range1 = Date(1, 1, 1)..Date(5, 5, 5)
+        val range2 = Date(2, 2, 2)..Date(3, 3, 3)
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            range1 < range2
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideData(): Array<Arguments> {
+            return arrayOf(
+                Arguments.of(
+                    Date(1, 1, 1)..Date(2, 2, 2),
+                    Date(3, 2, 2)..Date(3, 3, 3)
+                ),
+                Arguments.of(
+                    Date(1, 1, 1)..Date(2, 2, 2),
+                    Date(1, 2, 1)..Date(3, 3, 3)
+                ),
+            )
+        }
+    }
+
 }
